30be0def499d7281225f10e155673e85024c4028
giselledelgadill <97265938+giselledelgadill@users.noreply.github.com>
2022-05-12 19:58:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+difference=0;
+rightWristx=0;
+leftWristx=0;
+nosey=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(500,500);
@@ -8,12 +14,24 @@ function setup(){
 }
 function draw(){
     background('gray');
+    document.getElementById("money").innerHTML="width & height off the square will be = "+difference+"px";
+    fill('blue');
+    stroke('red');
+    square(nosex,nosey,difference);
 }
 function modelLoaded(){
 console.log('poseNet started');
 }
-function gotPoses(){
+function gotPoses(results){
 if(results.length>0){
     console.log(results);
+    nosex=results[0].pose.nose.x;
+    nosey=results[0].pose.nose.y;
+    console.log("nosex= "+nosex+"nosey= "+nosey);
+
+    leftWristx=results[0].pose.leftWrist.x;
+    rightWristx=results[0].pose.rightWrist.x;
+    difference=floor(leftWristx-rightWristx);
+    console.log("leftWristx= "+leftWristx+"rightWristx= "+"differecne= "+difference);
 }
 }
